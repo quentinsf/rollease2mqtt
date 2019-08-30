4e59411420328bec0319afa4d49c5b21da653462
Quentin Stafford-Fraser <quentin@pobox.com>
2019-08-30 20:48:22 +0100

    Getting closer. Need to fix MQTT updating.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 #
-# Experiments with talking to Rollease Acmeda hub over RS485
+# Experiments with talking to Rollease Acmeda hub over RS485.
+# Not very tidy yet.
+#
 # Requires Python 3.7 or later
 
 import asyncio
@@ -8,22 +10,84 @@ import rollease
 import logging
 import sys
 import time
+from hbmqtt.client import MQTTClient, ClientException
+from hbmqtt.mqtt.constants import QOS_1, QOS_2
 
 from typing import Optional, Tuple, List, Dict
 
 DEVICE = "/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_018DF044-if00-port0"
+MQTT_URL = "mqtt://hassio-mqtt:9qaHD6@192.168.0.31"
+MQTT_TOPIC_ROOT = "home-assistant/cover"  # followed by /motor_addr/command
+MQTT_COMMAND_TOPIC = "set"
+MQTT_POSITION_TOPIC = "position"
+MQTT_SET_POSITION_TOPIC = "set_position"
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+async def monitor_mqtt_requests(hub, mqtt_client):
+    # Subscribe to mqtt topics for motors
+
+    await mqtt_client.subscribe([
+        (f'{MQTT_TOPIC_ROOT}/{motor_addr}/#', QOS_1) for motor_addr in hub.motors
+    ])
+
+    while True:
+        log.debug("Waiting for MQTT messages")
+
+        # wait for an MQTT message on our topics
+        message = await mqtt_client.deliver_message()
+        packet = message.publish_packet
+        topic = packet.variable_header.topic_name
+        payload = packet.payload.data.decode()
+
+        log.info("MQTT received %s => %s", topic, payload)
+        if topic.startswith(MQTT_TOPIC_ROOT):
+            motor, subtopic = topic[len(MQTT_TOPIC_ROOT)+1:].split('/')
+            log.info(f"  motor {motor} subtopic {subtopic}")
+            if motor in hub.motors:
+                if subtopic == MQTT_COMMAND_TOPIC:
+                    if payload == "CLOSE":
+                        await hub.motors[motor].request_close()
+                    elif payload == "OPEN":
+                        await hub.motors[motor].request_open()
+                    elif payload == "STOP":
+                        await hub.motors[motor].request_stop()
+            else:
+                log.warning("Request for unknown motor {motor}")
+        else:
+            log.error("Topic not under expected topic root")
+
+
+async def update_mqtt_positions(hub, mqtt_client):
+    while True:
+        await asyncio.sleep(5)
+
+        for motor_addr in hub.motors:
+            position = str(hub.motors[motor_addr].travel_pc).encode()
+            topic = f"{MQTT_TOPIC_ROOT}/{motor_addr}/{MQTT_POSITION_TOPIC}"
+            log.debug("Sending position %s to topic %s", position, topic)
+            message = await mqtt_client.publish( topic, position )
+
 
 async def main():
+
+    mqtt_client = MQTTClient(client_id="rollease2mqtt")
+    await mqtt_client.connect(MQTT_URL)
+
     conn = rollease.AcmedaConnection(device=DEVICE, timeout=3)
 
-    # print("Move motor 2 to 50%")
-    # await conn.send_motor_cmd(hub, "002", cmd="m", data="050")
-
+    await conn.request_hub_info()
     
-    await asyncio.sleep(30)
+    await asyncio.sleep(8)
+    hub_addr, hub = next(iter(conn.hubs.items()))
+    
+    asyncio.create_task(update_mqtt_positions(hub, mqtt_client))
+    asyncio.create_task(monitor_mqtt_requests(hub, mqtt_client))
+
+    while True:
+        log.info("rollease2mqtt alive and waiiting")
+        await asyncio.sleep(300)
 
 asyncio.run(main())
--- rollease.py
+++ rollease.py
@@ -39,11 +39,15 @@ class Hub:
         self.conn = conn
         self.addr = addr
         self.motors: Dict[str, "Motor"] = {}
+
         log.info(f"Requesting motor info for hub {addr}")
         asyncio.create_task(self.request_motor_info())
 
     def __str__(self):
         return f"Hub {self.addr}"
+
+    # This is a motor command, but it's at this level because
+    # it allows use to find the motors and register them.
 
     async def request_motor_info(self, motor="000", cmd="v", data="?"):
         await self.conn.send_motor_cmd(self.addr, motor, cmd, data)
@@ -53,32 +57,41 @@ class Hub:
             return
         self.motors[addr] = Motor(self, addr)
 
-    def parse_motor_info(
+    def handle_uplink(self, delim: str, resp: str):
+        """
+        If an incoming message is from this hub,
+        handle the top-level delimiter (typically 'D')
+        and the rest of the resonse.
+        """
+
+        if delim == 'V':
+            log.info("Got address message from hub %s", self.addr)
+        elif delim == "A":
+            log.warning("We don't handle hub test/address change responses yet")
+        elif delim == 'D':
+            log.debug("Received motor message %s %s", delim, resp)
+            self.handle_motor_info(resp)
+
+    def _parse_motor_info(
         self, info: str
-    ) -> Tuple[str, Optional[str], Optional[str]]:
+    ) -> Tuple[str, str, str]:
         motor_addr = info[:3]
         command, remainder = None, None
-        if len(info) > 3:
-            command = info[3]
-        if len(info) > 4:
-            remainder = info[4:]
+        command = info[3]
+        remainder = info[4:]
         return motor_addr, command, remainder
 
     def handle_motor_info(self, resp: str):
-        motor_addr, command, remainder = self.parse_motor_info(resp)
+        motor_addr, command, remainder = self._parse_motor_info(resp)
         if motor_addr not in self.motors:
             self.register_motor(motor_addr)
 
         motor = self.motors[motor_addr]
-        if command == 'r' and remainder is not None:
-            motor.handle_position_info(remainder)
-        elif command == 'v' and remainder is not None:
-            motor.handle_version_info(remainder)
-        else:
-            log.warning("unknown motor command %s%s", command, remainder)
+        motor.handle_uplink(command, remainder)
 
 
 class Motor:
+
     def __init__(self, hub: Hub, addr: str):
         log.info(f"Registering motor {addr} on {hub}")
         self.hub = hub
@@ -87,32 +100,97 @@ class Motor:
         self.rotation_deg: Optional[str] = None
         self.version: Optional[str] = None
         log.info(f"Requesting position info for motor {addr} on hub {hub.addr}")
-        asyncio.create_task(self.request_position_info())
+        asyncio.create_task(self.request_current_position())
 
     def __str__(self):
         return f"motor {self.name} on {self.hub}"
 
-    async def request_position_info(self):
-        await self.hub.request_motor_info(motor=self.addr, cmd="r", data="?")
+    async def request_cmd(self, cmd: str = "r", data: str = ""):
+        await self.hub.request_motor_info(self.addr, cmd, data)
 
-    def parse_position_info(
-        self, info: str
-    ) -> Tuple[str, str]:
-        if 'b' not in info:
-            raise FormatError("Expected position info but didn't find a 'b'")
-        travel_pc, rotation_deg = info.split('b')
-        return travel_pc, rotation_deg
+    # Generic motor response handler
+
+    def handle_uplink(self, command: str, remainder: str):
+        if command == "U":
+            log.error("Can't get position/stroke not set")
+        elif command in ('>', '<') and remainder is not None:
+            self.handle_motion_position_info(remainder)
+        elif command == 'r' and remainder is not None:
+            self.handle_stop_position_info(remainder)
+        elif command == 'v' and remainder is not None:
+            self.handle_version_info(remainder)
+        else:
+            log.warning("unknown motor command %s%s", command, remainder)
+
+    # Version
 
     def handle_version_info(self, info: str):
         self.version = info
-        log.info("Recorded version of hub %s motor %s as '%s'", 
+        log.info("Recorded version of %s motor %s as '%s'", 
             self.hub, self.addr, self.version)
 
-    def handle_position_info(self, info:str):
-        self.travel_pc, self.rotation_deg = self.parse_position_info(info)
-        log.info("Recorded position of hub %s motor %s as %s%%, %s deg", 
-            self.hub, self.addr, self.travel_pc, self.rotation_deg)
+    # Position
 
+    def handle_motion_position_info(self, info:str):
+        self.travel_pc, self.rotation_deg = travel_pc, rotation_deg = info.split('b')
+        log.info("Recorded motion of %s motor %s from %s%%, %s deg", 
+            self.hub, self.addr, self.travel_pc, self.rotation_deg)
+        # TODO: callback
+
+    def handle_stop_position_info(self, info:str):
+        self.travel_pc, self.rotation_deg = travel_pc, rotation_deg = info.split('b')
+        log.info("Recorded position of %s motor %s as %s%%, %s deg", 
+            self.hub, self.addr, self.travel_pc, self.rotation_deg)
+        # TODO: callback
+
+    # Motior requests
+
+    async def request_close(self):
+        await self.request_cmd(cmd="c", data="")
+
+    async def request_open(self):
+        await self.request_cmd(cmd="o", data="")
+
+    async def request_stop(self):
+        await self.request_cmd(cmd="s", data="")
+
+    async def request_jog_down(self):
+        await self.request_cmd(cmd="cA", data="")
+
+    async def request_jog_up(self):
+        await self.request_cmd(cmd="oA", data="")
+
+    async def request_move_percent(self, pc: int):
+        await self.request_cmd(cmd="m", data="%03d" % pc)
+    
+    async def request_rotate_percent(self, pc: int):
+        await self.request_cmd(cmd="b", data="%03d" % pc)
+
+    async def request_move_preferred_position(self):
+        await self.request_cmd(cmd="f", data="")
+
+    async def request_motor_param(self):
+        await self.request_cmd(cmd="N", data="?")
+
+    async def request_current_position(self):
+        await self.request_cmd(cmd="r", data="?")
+
+    async def request_preferred_position(self):
+        await self.request_cmd(cmd="f", data="?")
+
+    async def request_motor_speed(self):
+        await self.request_cmd(cmd="pSc", data="?")
+
+    async def request_motor_voltage(self):
+        await self.request_cmd(cmd="pVc", data="?")
+
+    async def request_version(self):
+        await self.request_cmd(cmd="v", data="?")
+
+    async def request_position_limit_setting(self):
+        await self.request_cmd(cmd="pP", data="?")
+
+    # Haven't bothered with the motor limits etc yet
 
 class AcmedaConnection(object):
     def __init__(self, device: str, timeout: int = 5):
@@ -125,28 +203,12 @@ class AcmedaConnection(object):
         self.hubs: Dict[str, Hub] = {}
 
         # Start a watcher
-        asyncio.create_task(self.watch_updates())
+        asyncio.create_task(self.monitor_updates())
 
-    async def request_hub_info(self):
-        log.info("Ask hubs to report in")
-        await self.send_hub_cmd(hub="000", cmd="V", data="?")
+ 
+    # Low-level stuff to do with marchalling and parsing the messages
 
-    async def send_hub_cmd(self, hub: str = "000", cmd: str = "V", data: str = "?"):
-        cmd = "%03s%s%s" % (hub, cmd, data)
-        log.debug("sending hub command %s ", cmd)
-        await self.send_cmd(cmd)
-        log.debug("sent hub command %s ", cmd)
-
-    async def send_motor_cmd(
-        self, hub: str = "000", motor: str = "000", cmd: str = "v", data: str = "?"
-    ):
-        """
-        Note that non-integer parameters are bytestrings not strings.
-        """
-        cmd = "%03sD%03s%s%s" % (hub, motor, cmd, data)
-        return await self.send_cmd(cmd)
-
-    async def send_cmd(self, cmd: str) -> int:
+    async def _send_cmd(self, cmd: str) -> int:
         """
         Take command string, convert to bytes and send.
         Return number of bytes written
@@ -155,7 +217,26 @@ class AcmedaConnection(object):
         log.debug("  sending %s", cmd_bytes)
         return await self.ser.write_async(cmd_bytes)
 
-    async def get_response(self) -> str:
+    async def send_hub_cmd(self, hub: str = "000", cmd: str = "V", data: str = "?"):
+        """
+        Send the specified command and data to the specified hub.
+        """
+        cmd = "%03s%s%s" % (hub, cmd, data)
+        log.debug("  sending hub command %s ", cmd)
+        sent = await self._send_cmd(cmd)
+        log.debug("  sent hub command %s: %s bytes ", cmd, sent)
+
+    async def send_motor_cmd(
+        self, hub: str = "000", motor: str = "000", cmd: str = "v", data: str = "?"
+    ):
+        """
+        Send the specified command and data to the specified motor on the specified hub.
+        """
+        cmd = "%03sD%03s%s%s" % (hub, motor, cmd, data)
+        return await self._send_cmd(cmd)
+
+
+    async def _get_response(self) -> str:
         """
         Get the next response (up to a semicolon) as a string.
         Returns None on a timeout or invalid response.
@@ -163,12 +244,24 @@ class AcmedaConnection(object):
         resp = await self.ser.read_until_async(b";")
         log.debug("  got %s", resp)
         if resp is None:
-            raise TimeoutError("Timed out in get_response")
+            raise TimeoutError("Timed out in _get_response")
         return resp.decode()
+
+    def _parse_response(self, resp: str) -> Tuple[str, str, str]:
+        """
+        Split up a top-level response into the hub address,
+        the delimiter, and the bit after the delimiter.
+        Returns hub, delim, response.
+        """
+        if resp.startswith("!") and resp.endswith(";"):
+            hub, delim, resp = resp[1:4], resp[4], resp[5:-1]
+            return (hub, delim, resp)
+        else:
+            raise FormatError("Didn't find response delimited by '!' and ';'")
 
     async def response_iter(self, timeouts=10):
         """
-        An iterator for possibly several responses.
+        An iterator for retrieving possibly several responses.
         We don't usually have a way to know how many responses to 
         wait for, so we have to use a timeout.
         'timeouts' specifies the number of times we'll wait for the
@@ -182,36 +275,20 @@ class AcmedaConnection(object):
         responses = []
         timeout_count = 0
         while timeout_count < 2:
-            res = await self.get_response()
+            res = await self._get_response()
             if not res:
                 timeout_count += 1
             else:
-                yield self.parse_response(res)
+                yield self._parse_response(res)
 
-    def parse_response(self, resp: str) -> Tuple[str, str, str]:
-        """
-        Returns hub, delim, response.
-        """
-        if resp.startswith("!") and resp.endswith(";"):
-            hub, delim, resp = resp[1:4], resp[4], resp[5:-1]
-            return (hub, delim, resp)
-        else:
-            raise FormatError("Didn't find response delimited by '!' and ';'")
 
-    async def get_hubs(self) -> List[str]:
+    async def monitor_updates(self):
         """
-        Get the information about the first hub that responds.
+        Receive responses and pass them to the appropriate
+        party.
         """
-        log.debug("Find hub...")
-        await self.send_hub_cmd(hub="000", cmd="V", data="?")
-        # We're assuming just one hub here for now
-        hub_info = await self.get_response()
-        res, _, _ = self.parse_response(hub_info)
-        log.info("  hub info: %s", res)
-        return [res]
 
-    async def watch_updates(self):
-        print("Watching for updates")
+        log.info("Watching for updates")
         async for hub, delim, resp in self.response_iter():
             # If this is a hub we haven't seen before,
             # create it.
@@ -219,9 +296,13 @@ class AcmedaConnection(object):
             if hub not in self.hubs:
                 self.hubs[hub] = Hub(self, hub)
             
-            # Are we likely to have motor info next?
-            if delim == 'D':
-                self.hubs[hub].handle_motor_info(resp)
-                
+            self.hubs[hub].handle_uplink(delim, resp)    
             # print(f"  Hub {hub} response: {resp}")
 
+
+    async def request_hub_info(self):
+        """
+        Ask all hubs to report in with address info
+        """
+        log.info("Ask hubs to report in")
+        await self.send_hub_cmd(hub="000", cmd="V", data="?")
